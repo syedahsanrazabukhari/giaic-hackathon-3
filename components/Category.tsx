23a467159c69292401aa273a6e51bf8c0ed0e1ee
syedahsanrazabukhari <syedahsanrazabukhari10@gmail.com>
2025-01-21 20:58:23 +0500

    category problem solving

--- components/Category.tsx
+++ components/Category.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Category() {
   return (
     <>
-      <ul className="flex gap-x-11 text-[#726E8D] justify-center pt-5 max-sm:overflow-scroll max-sm:pl-[350px] max-sm:pr-10">
+      <ul className="flex gap-x-11 text-[#726E8D] justify-center pt-10 sm:pt-5 max-sm:overflow-scroll max-sm:pl-[350px] max-sm:pr-10 ">
         <li>
           <Link href="/products?category=plant%20pots">Plant pots</Link>
         </li>
