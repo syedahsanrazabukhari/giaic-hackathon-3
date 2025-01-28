ec1f34276e95ef2459517d138a45993a1fc259b9
syedahsanrazabukhari <syedahsanrazabukhari10@gmail.com>
2025-01-29 01:49:09 +0500

    apply colors

--- components/LoginIn.tsx
+++ components/LoginIn.tsx
@@ -3,7 +3,7 @@ import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 function LoginIn() {
     return (
         <>
-            <div className="max-sm:pl-3 max-sm:py-2 max-sm:text-[17px]"> 
+            <div className="max-sm:pl-3 max-sm:py-2 max-sm:text-[17px] text-black"> 
                 <SignedOut>
                     <SignInButton />
                 </SignedOut>
