209db3da12afc19e9d2921c1b62b89526c57e4d9
syedahsanrazabukhari <syedahsanrazabukhari10@gmail.com>
2025-02-02 16:11:25 +0500

    make resposive succes and acncel page

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -6,9 +6,9 @@ function success() {
     return (
         <>
             <Navbar />
-            <h1 className="pt-[10%] pl-60 w-full">YOUR ORDER HAS BEEN PLACED SUCCESSFULLY </h1>
+            <h1 className="pt-[10%] ml-[30px] sm:pl-60 ">YOUR ORDER HAS BEEN PLACED SUCCESSFULLY </h1>
             <Link href="/">
-                <button className="bg-[#2A254B] w-28 mx-[45%] my-10 text-white px-6 py-3 rounded hover:bg-[#393552] transition-colors">
+                <button className="bg-[#2A254B] mx-[25%] sm:mx-[45%] my-10 text-white px-6 py-3 rounded hover:bg-[#393552] transition-colors">
                     Go Back
                 </button>
             </Link>
